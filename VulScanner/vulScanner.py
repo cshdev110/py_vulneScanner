ce7bac011bf7d3b6dfc508f85ea33ee34a104c50
cscode <cscode@labkcs.code>
2024-06-05 23:15:47 +1000

    Extracting HTML and parsing URL using beautifulsoup and urllib.parse.

--- VulScanner/vulScanner.py
+++ VulScanner/vulScanner.py
@@ -2,13 +2,29 @@
 
 
 import requests
+from urllib import parse
 from bs4 import BeautifulSoup
 
 def request(url):
     try:
-        return requests.get("http://" + url)
+        return requests.get(url)
     except requests.exceptions.ConnectionError:
         pass
 
 
-#at two minutes from the tutorial
+target_url = "http://192.168.56.102/mutillidae/index.php?page=dns-lookup.php"
+response = request(target_url)
+# print(response.content.decode())
+
+parsed_html = BeautifulSoup(response.content.decode())
+forms_list = parsed_html.findAll("form")
+# print(forms_list)
+
+for form in forms_list:
+
+    print(parse.urljoin(target_url, form.get("action")))
+    print(form.get("method"))
+    
+    inputs_list = form.findAll('input')
+    for input in inputs_list:
+        print(input.get("name"))
