6d91fdd8b2a156ee289f710989b80209960b0fdf
cscode <cscode@labkcs.code>
2024-08-10 22:09:18 +1000

    Create session, ignore link login out, extract forms, submit forms.

--- VulScanner/scanner.py
+++ VulScanner/scanner.py
@@ -3,15 +3,18 @@
 import requests
 import re
 from urllib import parse
+from bs4 import BeautifulSoup
 
 
 class Scanner:
-    def __init__(self, uri):
+    def __init__(self, uri, links_to_ignore):
+        self.session = requests.Session()
         self.target_uri = uri
         self.target_links = []
+        self.links_to_ignore = links_to_ignore
 
     def extract_links_from(self, uri):
-        response = requests.get(uri)
+        response = self.session.get(uri)
         return re.findall('(?:href=")(.*?)"', response.content.decode('utf-8', 'ignore'))
     
     def crawl(self, uri=None):
@@ -26,7 +29,35 @@ class Scanner:
             if "#" in link:
                 link = link.split("#")[0]
 
-            if self.target_uri in link and link not in self.target_links:
+            if self.target_uri in link and link not in self.target_links and link not in self.links_to_ignore:
                 self.target_links.append(link)
                 print(link)
                 self.crawl(link)
+
+
+    def extract_forms(self, uri):
+        response = self.session.get(uri)
+        parsed_html = BeautifulSoup(response.content.decode("utf-8", "ignore"), features="html.parser")
+        return parsed_html.findAll("form")
+    
+
+    def submit_form(self, form, value, uri):
+        action = form.get("action")
+        post_uri = parse.urljoin(uri, action)
+        method = form.get("method")
+
+        inputs_list = form.findAll("input")
+        post_data = {}
+        for input in inputs_list:
+            input_name = input.get("name")
+            input_type = input.get("type")
+            input_value = input.get("value")
+            if input_type == "text":
+                input_value = value
+
+            post_data[input_name] = input_value
+        
+        if method == "post":
+            return self.session.post(post_uri, data=post_data)
+        
+        return self.session.get(post_uri, params=post_data)
--- VulScanner/vul_scanner.py
+++ VulScanner/vul_scanner.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 
 import scanner
+#import requests
 
-target_uri = "http://192.168.56.102/mutillidae/"
-vuln_scanner = scanner.Scanner(target_uri)
-vuln_scanner.crawl()
+##Testing
+# target_uri = "http://192.168.56.102/mutillidae/"
+
+target_uri = "http://192.168.56.102/dvwa/"
+links_to_ignore = ["http://192.168.56.102/dvwa/logout.php"]
+data_dict = {"username": "admin", "password": "password", "Login": "submit"}
+# Every usege of requests, it logins and closes the session immediately logging out removing all cookies. 
+# That is an issue as the session is required to stay open.
+#response = requests.post(target_uri, data=data_dict)
+
+
+vuln_scanner = scanner.Scanner(target_uri, links_to_ignore)
+vuln_scanner.session.post(target_uri + "login.php", data=data_dict)
+# vuln_scanner.crawl()
+forms = vuln_scanner.extract_forms("http://192.168.56.102/dvwa/vulnerabilities/xss_r/")
+print(forms)
+
+response = vuln_scanner.submit_form(forms[0], "test", "http://192.168.56.102/dvwa/vulnerabilities/xss_r/")
+print(response.content.decode("utf-8", "ignore"))
